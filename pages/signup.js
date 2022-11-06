2fc475aee5f6fc50f8b289ba8e65ea9e17d1e95a
naufalfirdaus <naufalfirdausa@gmail.com>
2022-11-06 21:59:01 +0700

    main

--- pages/signup.js
+++ pages/signup.js
@@ -1,6 +1,6 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import { useSelector, useDispatch } from 'react-redux';
-import { useFormik, ErrorMessage } from "formik";
+import { useDispatch } from 'react-redux';
+import { useFormik } from "formik";
 import { doAddSignupRequest } from './redux-saga/Action/UsrAction';
 import { useRouter } from 'next/router';
 import { LockClosedIcon } from '@heroicons/react/solid'
@@ -10,7 +10,6 @@ import Link from 'next/link';
 export default function signup() {
   const dispatch = useDispatch();
   const router = useRouter()
-  const { message, isLoggedIn } = useSelector(state => state.usrStated);
   const validationSchema = Yup.object().shape({
     FirstName: Yup.string().required('First Name is required'),
     LastName: Yup.string().required('Last Name is required'),
@@ -22,6 +21,13 @@ export default function signup() {
       .min(3)
       .max(10)
       .required('Password is required'),
+    confirmPassword: Yup.string().when("password", {
+      is: val => (val && val.length > 0 ? true : false),
+      then: Yup.string().oneOf(
+        [Yup.ref("password")],
+        "Both password need to be the same"
+      )
+    })
   });
 
   const formik = useFormik({
@@ -33,6 +39,7 @@ export default function signup() {
       LastName: "",
       Number: "",
       PontyCode: "",
+      confirmPassword: "",
       RoleId: 1
     },
     validationSchema: validationSchema,
@@ -56,8 +63,8 @@ export default function signup() {
     <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
         <div>
-        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign up to your account</h2>
-        <p className="mt-2 text-center text-sm text-gray-600">
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign up to your account</h2>
+          <p className="mt-2 text-center text-sm text-gray-600">
             Or{' '}
             <Link href="/signin" className="font-medium text-indigo-600 hover:text-indigo-500">
               sign in your account
@@ -65,12 +72,8 @@ export default function signup() {
           </p>
         </div>
         <form className="mt-8 space-y-6" action="#" method="POST">
-          <input type="hidden" name="remember" defaultValue="true" />
           <div className="rounded-md shadow-sm -space-y-px">
-          <div>
-              <label htmlFor="" className="sr-only">
-                First Name
-              </label>
+            <div>
               <input
                 id="FirstName"
                 name="FirstName"
@@ -81,15 +84,12 @@ export default function signup() {
                 autoComplete="FirstName"
                 required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                placeholder="FirstName"
+                placeholder="First Name"
               />
               {formik.touched.FirstName && formik.errors.FirstName ?
                 <span className="mt-2 text-sm text-red-600">{formik.errors.FirstName}</span> : null}
             </div>
             <div>
-              <label htmlFor="" className="sr-only">
-                Last Name
-              </label>
               <input
                 id="LastName"
                 name="LastName"
@@ -99,16 +99,13 @@ export default function signup() {
                 onBlur={formik.handleBlur}
                 autoComplete="LastName"
                 required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                placeholder="LastName"
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Last Name"
               />
               {formik.touched.LastName && formik.errors.LastName ?
                 <span className="mt-2 text-sm text-red-600">{formik.errors.LastName}</span> : null}
             </div>
             <div>
-              <label htmlFor="email-address" className="sr-only">
-                Username
-              </label>
               <input
                 id="username"
                 name="username"
@@ -118,16 +115,13 @@ export default function signup() {
                 onBlur={formik.handleBlur}
                 autoComplete="username"
                 required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Username"
               />
               {formik.touched.username && formik.errors.username ?
                 <span className="mt-2 text-sm text-red-600">{formik.errors.username}</span> : null}
             </div>
             <div>
-              <label htmlFor="email-address" className="sr-only">
-                Email Address
-              </label>
               <input
                 id="email"
                 name="email"
@@ -137,73 +131,75 @@ export default function signup() {
                 onBlur={formik.handleBlur}
                 autoComplete="email"
                 required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                placeholder="email"
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Email Address"
               />
               {formik.touched.email && formik.errors.email ?
                 <span className="mt-2 text-sm text-red-600">{formik.errors.email}</span> : null}
             </div>
             <div>
-              <label htmlFor="" className="sr-only">
-                Code Telp.
-              </label>
-              <input
+              <select
                 id="PontyCode"
                 name="PontyCode"
-                type="PontyCode"
+                type="select"
                 value={formik.values.PontyCode}
                 onChange={formik.handleChange}
                 onBlur={formik.handleBlur}
                 autoComplete="PontyCode"
                 required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                placeholder="PontyCode"
-              />
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Code Telp."
+              >
+                <option value="Home" label="Home">
+                  Home
+                </option>
+                <option value="Cell" label="Cell">
+                  Cell
+                </option>
+              </select>
               {formik.touched.PontyCode && formik.errors.PontyCode ?
                 <span className="mt-2 text-sm text-red-600">{formik.errors.PontyCode}</span> : null}
             </div>
             <div>
-              <label htmlFor="" className="sr-only">
-                Number Telp.
-              </label>
               <input
                 id="Number"
                 name="Number"
-                type="Number"
+                type="text"
                 value={formik.values.Number}
                 onChange={formik.handleChange}
                 onBlur={formik.handleBlur}
                 autoComplete="Number"
                 required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                placeholder="Number"
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Number Telp."
               />
               {formik.touched.Number && formik.errors.Number ?
                 <span className="mt-2 text-sm text-red-600">{formik.errors.Number}</span> : null}
             </div>
             <div>
-              <label htmlFor="" className="sr-only">
-                Role Type
-              </label>
-              <input
+              <select
                 id="RoleId"
                 name="RoleId"
-                type="RoleId"
+                type="select"
                 value={formik.values.RoleId}
                 onChange={formik.handleChange}
                 onBlur={formik.handleBlur}
                 autoComplete="RoleId"
                 required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                placeholder="RoleId"
-              />
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Role Type"
+              >
+                <option value="11">
+                  Outsource
+                </option>
+                <option value="1">
+                  Candidat
+                </option>
+              </select>
               {formik.touched.RoleId && formik.errors.RoleId ?
                 <span className="mt-2 text-sm text-red-600">{formik.errors.RoleId}</span> : null}
             </div>
             <div>
-              <label htmlFor="password" className="sr-only">
-                Password
-              </label>
               <input
                 id="password"
                 name="password"
@@ -213,14 +209,28 @@ export default function signup() {
                 onBlur={formik.handleBlur}
                 autoComplete="current-password"
                 required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Password"
               />
               {formik.touched.password && formik.errors.password ?
                 <span className="mt-2 text-sm text-red-600">{formik.errors.password}</span>
                 : null}
-              {message ?
-                <span className="mt-2 text-sm text-red-600">{message}</span>
+            </div>
+            <div>
+              <input
+                id="confirmPassword"
+                name="confirmPassword"
+                type="password"
+                value={formik.values.confirmPassword}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                autoComplete="current-password"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Confirm Password"
+              />
+              {formik.touched.confirmPassword && formik.errors.confirmPassword ?
+                <span className="mt-2 text-sm text-red-600">{formik.errors.confirmPassword}</span>
                 : null}
             </div>
           </div>
