41ab5d4215618470041bcbac878afbd57448dbc6
kflmattukz <kflmattukz@gmail.com>
2022-12-05 12:00:45 +0800

    batch new

--- pages/app/batch/index.js
+++ pages/app/batch/index.js
@@ -3,12 +3,16 @@ import Page from '../../component/commons/Page';
 import AppLayout from '../../component/layout/AppLayout';
 
 export default function Batch() {
-  let navigate = useRouter()
+  let navigate = useRouter();
   return (
     <AppLayout>
-      <Page title='Batch' titleButton='Create' onClick={() => navigate('/app/batch/new')}>
+      <Page
+        title='Batch'
+        titleButton='Create'
+        onClick={() => navigate('/app/batch/new')}
+      >
         <h2>Test</h2>
       </Page>
     </AppLayout>
-  )
+  );
 }
---
+++ pages/app/batch/new/index.jsx
@@ -0,0 +1,9 @@
+import AppLayout from '../../../component/layout/AppLayout';
+
+export default function BatchNew() {
+  return (
+    <AppLayout>
+      <div>new batch</div>
+    </AppLayout>
+  );
+}
